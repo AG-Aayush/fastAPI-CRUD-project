53e1463eac6a71a3714eb46848977c9ca7f8e8d8
Aayush Gnawali <AG-Aayush>
2026-05-16 14:56:57 +0545

    Updated routes  for authentication

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,42 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db import get_db
-from app.schemas import NoteCreate, NoteResponse
+from app.schemas import NoteCreate, NoteResponse, UserCreate, UserLogin, Token
 from app import crud
+from app.crud import create_user, get_user_by_email
+from app.auth import verify_password,create_access_token
 
-router = APIRouter(prefix="/notes", tags=['Notes'])
+
+router = APIRouter(prefix="/notes", tags=['Notes', 'Authentication'])
+
+@router.post("/register")
+async def register(user: UserCreate, db: AsyncSession = Depends(get_db)):
+    existing_user = await get_user_by_email(db, user.email)
+
+    if existing_user:
+        raise HTTPException(status=404, detail="Email already registered")
+    return await create_user(db, user)
+
+@router.post("/login", response_model=Token)
+async def login(user: UserLogin, db: AsyncSession=Depends(get_db)):
+    db_user = await get_user_by_email(db, user.email)
+
+    if not db_user:
+        raise HTTPException(status_code=404, detail="Invalid Credentials")
+    
+    if not verify_password(user.password, db_user.password):
+        raise HTTPException(status_code=404, detail="Invalid Credentials")
+    
+    access_token = create_access_token(
+        data={
+            "sub":db_user.email
+        }
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
 
 @router.post('/', response_model=NoteResponse)
 async def create_note(note: NoteCreate, db: AsyncSession = Depends(get_db)):
