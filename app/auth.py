e7076e8990aae2efe1b1a98cc08e1beb659823a5
Aayush Gnawali <AG-Aayush>
2026-05-16 13:53:59 +0545

    Implemented JWT authentication

---
+++ app/auth.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timedelta
+from json import JWTError, jwt
+from passlib.context import CryptoContex
+
+SECRET_KEY = 'mysecretkey'
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptoContex(
+    schemas= ['bcrypt'],
+    deprecated = "auto"
+)
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(
+        plain_password, hashed_password
+    )
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
