3bdfe505753c71f5f892ee18dfc272e840afe0b7
Aayush Gnawali <AG-Aayush>
2026-05-16 13:57:17 +0545

    Updated Schemas for authentication

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 class NoteCreate(BaseModel):
     title: str
